72c8417ce5eacf007599def39dc54c743a87da85
Diego Barrios Romero <eldruin@gmail.com>
2019-06-15 12:56:03 +0200

    Add initial version

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! This is a platform agnostic Rust driver for the MCP794xx real-time clock
+//! / calendar family, based on the [`embedded-hal`] traits.
+//!
+//! [`embedded-hal`]: https://github.com/rust-embedded/embedded-hal
+
+#![deny(unsafe_code, missing_docs)]
+#![no_std]
